000553040d200a6db560e4a4bafddf3c0821a365
baditude <kaushik.gla_cs19@gla.ac.in>
2021-11-14 19:09:16 +0530

    php file

--- classes/signup.php
+++ classes/signup.php
@@ -9,30 +9,56 @@ class signup
         {
             if (empty($value))
             {
-                $error =$error. $key." is empty!<br>";
+                $this->error =$this->error. $key." is empty!<br>";
 
             }
         }
-        if ($error == "")
+        if ($this->error == "")
         {
             //NO ERROR
             $this->create_user($data);
         }
         else
         {
-            return $error;
+            return $this->error;
         }
     }
 
     public function create_user($data)
     {
+        
+        $fname = $data['fname'];
+        $lname = $data['lname'];
+        $gender = $data['gender'];
+        $email = $data['email'];
+        $password = $data['password'];
+
+        $url =strtolower( $fname). ".".strtolower($lname);
+        $userid = this->create_userid();
+
         $query = "insert into users
         (userid,fname,lname,gender,email,password,url) 
         values 
-        ('$userid','$fname','$lname','$gender','$email','$password','$url')"
-        $DB= new database();
-        $DB->save($query);
+        ('$userid','$fname','$lname','$gender','$email','$password','$url')";
+        
+        return $query;
+        // $DB= new database();
+        // $DB->save($query);
 
+    }
+   
+
+    private function create_userid()
+    {
+       $length = rand (4,19);
+       $number = "";
+       for ($i = 0; $i < $length; $i++ )
+       {
+           $new_rand = rand(0,9);
+           $number = $number . $new_rand;
+       }
+
+       return $number;
     }
 }
 
